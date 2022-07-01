e4dc79a8046424cc45fbb90b5f18f89d1aaec8b4
Sally Diament <sally.barzilai@gmail.com>
2022-07-01 17:51:20 -0300

    incluindo uma parte do requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,59 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">
-        Login
-      </div>
-    );
+    state ={
+      inputName: '',
+      isSaveButtonDisabled: true,
+    }
+
+validate = () => {
+  const { inputName } = this.state;
+  if (inputName.length > 2) {
+    this.setState({ isSaveButtonDisabled: false });
+  } else {
+    this.setState({ isSaveButtonDisabled: true });
   }
+}
+
+    onInputChange = (event) => {
+      const { target } = event;
+      this.setState({
+        [target.name]: target.value,
+      }, () => this.validate());
+    }
+
+   handleClick = async () => {
+     const { inputName } = this.state;
+     await createUser({ name: inputName });
+   }
+
+   render() {
+     const { isSaveButtonDisabled, inputName } = this.state;
+     return (
+       <div data-testid="page-login">
+         <label htmlFor="inputName">
+           Insira seu nome de usuário:
+           <input
+             type="text"
+             data-testid="login-name-input"
+             name="inputName"
+             id="inputName"
+             value={ inputName }
+             onChange={ this.onInputChange }
+           />
+         </label>
+         <button
+           type="submit"
+           data-testid="login-submit-button"
+           disabled={ isSaveButtonDisabled }
+           onClick={ this.handleClick }
+         >
+           Entrar
+         </button>
+       </div>
+     );
+   }
 }
 
 export default Login;

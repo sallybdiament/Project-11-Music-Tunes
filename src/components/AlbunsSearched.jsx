625e02ad9ecab33af6b41e2ea95da701f5dac607
Sally Diament <sally.barzilai@gmail.com>
2022-07-02 00:26:38 -0300

    inserindo comentarios com minhas dúvidas

--- src/components/AlbunsSearched.jsx
+++ src/components/AlbunsSearched.jsx
@@ -5,15 +5,18 @@ import Card from './Card';
 
 class AlbunsSearched extends React.Component {
     state = {
+    // Fiquei na dúvida se inicio como string vazia ou como array vazio?
       resultsAlbuns: '',
     }
 
+    // Será que aqui precisa de componentDidMount? Ou didUpate?
     showAlbuns = async () => {
       const { searchArtist } = this.state;
       this.setState({ isLoading: true });
       const result = await searchAlbumsAPI(searchArtist);
       this.setState({
         searchArtist: '',
+        // Me parece que não estou inserindo corretamente os dados no array:
         resultsAlbuns: result,
         isLoading: false,
       });
@@ -26,6 +29,7 @@ class AlbunsSearched extends React.Component {
           { isLoading
             ? <Carregando />
             : (
+            // Fazer outro if aqui que se o resultsAlbuns.length < 0, retorna que nenhum album foi encontrado.
               <div>
                 <p>
                   Resultado de álbuns de:

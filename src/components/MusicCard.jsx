992a9e56e7cc97ce967ebc1d6c8eebc0aebabac4
Sally Diament <sally.barzilai@gmail.com>
2022-07-05 14:46:53 -0300

    passando os testes do requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from '../pages/Carregando';
 
 class MusicCard extends React.Component {
+  state = {
+    favorite: false,
+    isLoading: false,
+  }
+
+  onCheckboxChange = async () => {
+    this.setState({ isLoading: true });
+    const objeto = {
+      test: 'testes',
+    };
+    await addSong(objeto);
+    this.setState({ isLoading: false });
+  }
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { trackName, previewUrl, trackId } = this.props;
+    const { favorite, isLoading } = this.state;
     return (
       <article>
         <p>{trackName}</p>
@@ -15,6 +32,18 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="favorite">
+          Favorita
+          <input
+            type="checkbox"
+            data-testid={ `checkbox-music-${trackId}` }
+            name="favorite"
+            id="favorite"
+            value={ favorite }
+            onChange={ this.onCheckboxChange }
+          />
+        </label>
+        { isLoading && <Carregando /> }
       </article>
     );
   }
@@ -23,6 +52,7 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -51,6 +51,7 @@ class Album extends React.Component {
                   <MusicCard
                     trackName={ music.trackName }
                     previewUrl={ music.previewUrl }
+                    trackId={ music.trackId }
                   />
                 </div>
               )) }

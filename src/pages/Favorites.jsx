105bac7cd0c3f6b769025678d0875ea1584d27f1
Sally Diament <sally.barzilai@gmail.com>
2022-07-05 22:57:23 -0300

    incluindo o requisito 13

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class Favorites extends React.Component {
+  state = {
+    isLoading: false,
+    favoriteSongs: [],
+  }
+
+  componentDidMount = async () => {
+    this.setState({ isLoading: true });
+    const favorites = await getFavoriteSongs();
+    const favoriteSongs = favorites;
+    this.setState({ favoriteSongs, isLoading: false });
+  }
+
   render() {
+    const { isLoading, favoriteSongs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
         Favorites
+        { isLoading ? <Carregando />
+          : (
+            favoriteSongs.map((music) => (
+              <div key={ music.trackId }>
+                <MusicCard
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                  trackId={ music.trackId }
+                  objeto={ music }
+                  // favorita={ true }
+                />
+              </div>
+            ))
+          )}
       </div>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Profile extends React.Component {
+  state = {
+    userName: '',
+    userEmail: '',
+    userDescription: '',
+    userImg: '',
+    isLoading: true,
+  }
+
+  componentDidMount = async () => {
+    const result = await getUser();
+    console.log(result);
+    this.setState({
+      userName: result.name,
+      userEmail: result.email,
+      userDescription: result.description,
+      userImg: result.image,
+    });
+    this.setState({ isLoading: false });
+  };
+
   render() {
+    const { isLoading, userName, userEmail, userDescription, userImg } = this.state;
     return (
-      <div data-testid="page-profile">
+      <div>
         <Header />
-        Profile
+        { isLoading
+          ? <Carregando />
+          : (
+            <div>
+              <p>{ userName }</p>
+              <p>{ userEmail }</p>
+              <p>{ userDescription }</p>
+              <img data-testid="profile-image" src={ userImg } alt={ userName } />
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          )}
       </div>
     );
   }

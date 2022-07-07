41a42376a8d74071ba62187bb4087719369c3a70
Sally Diament <sally.barzilai@gmail.com>
2022-07-06 21:53:34 -0300

    tentando o requisito 14

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
-import { createUser, getUser } from '../services/userAPI';
+import { updateUser, getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 
 class ProfileEdit extends React.Component {
@@ -11,38 +12,59 @@ class ProfileEdit extends React.Component {
     userImg: '',
     isLoading: false,
     isSavebuttonDisabled: true,
+    redirect: false,
   }
 
-  componentDidMount = async () => {
-    await createUser({
-      email: 'test@test.com',
-      description: 'teste',
-      image: 'teste',
-    });
-    this.setState({ isLoading: true });
-    const result = await getUser();
-    this.setState({ isLoading: false });
-    console.log(result);
-    this.setState({
-      userName: result.name,
-      userEmail: result.email,
-      userDescription: result.description,
-      userImg: result.image,
-    });
+  componentDidMount = () => {
+    this.resgatandoInfoUsuarios();
   };
 
-  validate = () => {
+resgatandoInfoUsuarios = async () => {
+  this.setState({ isLoading: true });
+  const result = await getUser();
+  this.setState({ isLoading: false });
+  // console.log(result);
+  this.setState({
+    userName: result.name,
+    userEmail: result.email,
+    userDescription: result.description,
+    userImg: result.image,
+  });
+}
+
+  onInputChange = (event) => {
+    const { target: { id, value } } = event;
+    this.setState({
+      [id]: value,
+    });
     const { userName, userEmail, userDescription, userImg } = this.state;
-    if (userName && userEmail && userDescription && userImg) {
-      this.setState = { isSavebuttonDisabled: false };
+    console.log(userName);
+    if (userName.length > 0
+      && userEmail.length > 0
+      && userImg.length > 0
+      && userDescription.length > 0
+    ) {
+      this.setState({ isSavebuttonDisabled: false });
+    } else {
+      this.setState({ isSavebuttonDisabled: true });
     }
   }
 
-  onInputChange = (event) => {
-    const { target } = event;
-    this.setState({
-      [target.name]: target.value,
-    }, () => this.validate());
+  handleClick = async () => {
+    const {
+      userName,
+      userEmail,
+      userDescription,
+      userImg,
+    } = this.state;
+    const objeto = {
+      name: userName,
+      email: userEmail,
+      image: userImg,
+      description: userDescription,
+    };
+    await updateUser(objeto);
+    this.setState({ redirect: true });
   }
 
   render() {
@@ -53,6 +75,7 @@ class ProfileEdit extends React.Component {
       userDescription,
       userImg,
       isSavebuttonDisabled,
+      redirect,
     } = this.state;
     return (
       <div data-testid="page-profile-edit">
@@ -60,68 +83,71 @@ class ProfileEdit extends React.Component {
         <p>Profile Edit</p>
         { isLoading ? <Carregando /> : null }
         <div>
-          <p>{ userName }</p>
+          {/* <p>{ userName }</p>
           <p>{ userEmail }</p>
           <p>{ userDescription }</p>
-          <img data-testid="profile-image" src={ userImg } alt={ userName } />
+          <img data-testid="profile-image" src={ userImg } alt={ userName } /> */}
           <h1>Formulário para editar perfil:</h1>
-          <label htmlFor="userName">
-            Alterar nome de usuário:
-            <input
-              type="text"
-              data-testid="edit-input-name"
-              name="userName"
-              id="userName"
-              value={ userName }
-              // placeholder={ userName }
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <label htmlFor="userEmail">
-            Altere seu email:
-            <input
-              type="email"
-              data-testid="edit-input-email"
-              name="userEmail"
-              id="userEmail"
-              // placeholder={ userEmail }
-              value={ userEmail }
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <label htmlFor="userDescription">
-            Altere sua descrição:
-            <input
-              type="text"
-              data-testid="edit-input-description"
-              name="userDescription"
-              id="userDescription"
-              value={ userDescription }
-              // placeholder={ userDescription }
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <label htmlFor="userImg">
-            Altere sua foto:
-            <input
-              type="text"
-              data-testid="edit-input-image"
-              name="userImg"
-              id="userImg"
-              value={ userImg }
-              // placeholder={ userImg }
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <button
-            type="submit"
-            data-testid="edit-button-save"
-            disabled={ isSavebuttonDisabled }
-            // onClick={ this.handleClick }
-          >
-            Salvar
-          </button>
+          <form>
+            <label htmlFor="userName">
+              Alterar nome de usuário:
+              <input
+                type="text"
+                data-testid="edit-input-name"
+                name="userName"
+                id="userName"
+                value={ userName }
+                // placeholder={ userName }
+                onChange={ this.onInputChange }
+              />
+            </label>
+            <label htmlFor="userEmail">
+              Altere seu email:
+              <input
+                type="email"
+                data-testid="edit-input-email"
+                name="userEmail"
+                id="userEmail"
+                // placeholder={ userEmail }
+                value={ userEmail }
+                onChange={ this.onInputChange }
+              />
+            </label>
+            <label htmlFor="userDescription">
+              Altere sua descrição:
+              <input
+                type="text"
+                data-testid="edit-input-description"
+                name="userDescription"
+                id="userDescription"
+                value={ userDescription }
+                // placeholder={ userDescription }
+                onChange={ this.onInputChange }
+              />
+            </label>
+            <label htmlFor="userImg">
+              Altere sua foto:
+              <input
+                type="text"
+                data-testid="edit-input-image"
+                name="userImg"
+                id="userImg"
+                value={ userImg }
+                // placeholder={ userImg }
+                onChange={ this.onInputChange }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="edit-button-save"
+              disabled={ isSavebuttonDisabled }
+              onClick={ this.handleClick }
+            >
+              Salvar
+            </button>
+          </form>
         </div>
+        { redirect && <Redirect to="/search" />}
       </div>
     );
   }
